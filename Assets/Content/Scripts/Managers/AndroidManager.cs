e3d0ec851b1e716271bfbff8a76d9a1d17c27941
rhylvin2021 <rhylv@DESKTOP-8S1K0K5>
2022-07-29 17:23:08 +0800

    initial push

---
+++ Assets/Content/Scripts/Managers/AndroidManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem;
+
+public class AndroidManager : Singleton<AndroidManager>
+{
+    #region Public Variables
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Public Functions
+    #endregion
+
+    #region Private Fuctions
+    private void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.ACTIVITY_RECOGNITION"))
+        {
+            Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
+        }
+        InputSystem.EnableDevice(StepCounter.current);
+
+    }
+    private void Update()
+    {
+        try
+        {
+            if (StepCounter.current.enabled)
+            {
+                StepCounter.current.samplingFrequency = 1;
+                var stepValue = StepCounter.current.stepCounter.ReadValue();
+                //stepsValue += stepValue;
+                //stepText.text = stepValue.ToString();
+            }
+            else
+            {
+                //stepText.text = "not enabled";
+            }
+        }
+        catch (Exception e)
+        {
+            //stepText.text = $"error: {e.Message}";
+        }
+    }
+    #endregion
+}
---
+++ Assets/Content/Scripts/Managers/IOSManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeliefEngine.HealthKit;
+using System;
+
+public class IOSManager : Singleton<IOSManager>
+{
+    #region Public Variables
+    public HealthKitDataTypes types;
+    #endregion
+
+    #region Private Variables
+
+    private HealthStore healthStore;
+    private bool reading = false;
+    #endregion
+
+    #region Public Functions
+    public void ReadPedometer()
+    {
+        DateTimeOffset start = DateTimeOffset.UtcNow;
+
+        if (!reading)
+        {
+            int steps = 0;
+            this.healthStore.BeginReadingPedometerData(start, delegate (List<PedometerData> data, Error error) {
+                foreach (PedometerData sample in data)
+                {
+                    steps += sample.numberOfSteps;
+                }
+                //this.resultsLabel.text = string.Format("{0}", steps);
+            });
+            //buttonLabel.text = "Stop reading";
+            reading = true;
+        }
+        else
+        {
+            this.healthStore.StopReadingPedometerData();
+            //buttonLabel.text = "Start reading";
+            reading = false;
+        }
+    }
+    #endregion
+
+    #region Private Fuctions
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            this.healthStore = this.GetComponent<HealthStore>();
+            this.healthStore.Authorize(this.types);
+        }
+    }
+    #endregion
+}
