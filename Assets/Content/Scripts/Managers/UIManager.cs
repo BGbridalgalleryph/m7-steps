eb0c1db747debb2914ff86201b19e4432a04bb84
rhylvin2021 <rhylv@DESKTOP-8S1K0K5>
2022-08-01 13:14:16 +0800

    added uimanager

---
+++ Assets/Content/Scripts/Managers/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    #region Public Variables
+    public Text pedometerText;
+    public Text pedometerTextButtonText;
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Public Functions
+    #region UI Button Functions
+    public void Pedometer()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidManager.Instance.ReadPedometer();
+        }
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            IOSManager.Instance.ReadPedometer();
+        }
+    }
+    #endregion
+
+    #endregion
+
+    #region Private Fuctions
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+    }
+    #endregion
+}
